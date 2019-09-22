961f2e5faa14345596afb5c49b035ae615f5580b
Yifan Dang <32559842+ydang1@users.noreply.github.com>
2019-09-22 12:38:40 -0400

    Update small business.R

--- code/small business.R
+++ code/small business.R
@@ -3,17 +3,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(data.table)
 small_business <- fread("Maryland SBDC Data 4 UMD Data Challenge 2019.csv", sep=",", header = TRUE, stringsAsFactors = TRUE)
 
-levels(small_business$`Service Center`)
-levels(small_business$County)
-levels(small_business$`Initial Services Sought at First Visit`)
-levels(small_business$`Attended Group Training?`)
-levels(small_business$`Business Status`)
-levels(small_business$`Impact: Started Business`)
-levels(small_business$`NAICS code`)
-levels(small_business$`Ownership Gender`)
-levels(small_business$`Owner's Hispanic Origin`)
-levels(small_business$`Owner's Race`)
-
+for (i in columns(small_business)){
+  print (levels(small_business$i)
+  }
 success <- small_business[`Impact: Started Business`=="Yes"&`Business Status`=="Started with SBDC"]
 fail <- small_business[`Impact: Started Business`=="No"&`Business Status`=="Pre-venture/Nascent"]
 SBDC <- small_business[`Business Status`=="Started with SBDC"]
